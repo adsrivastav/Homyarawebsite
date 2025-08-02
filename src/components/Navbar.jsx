4df93ad9f2c33245fde4eae22349962b47531a49
Adarsh Rakesh Srivastav <adsrivastav179@gmail.com>
2025-08-02 23:19:31 +0530

    Update Navbar component and styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,39 @@
 // src/components/Navbar.jsx
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaBars, FaTimes } from "react-icons/fa";
 import logo from '../assets/mainlogo.jpg';
 import { useCart } from './CartContext';
 
 function Navbar() {
   const { cartItems } = useCart();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/" className="logo-link">Homyara Services and Solutions</Link>
+        <Link to="/" className="logo-link" onClick={closeMenu}>Homyara Services and Solutions</Link>
         <img src={logo} alt="Homyara Logo" className="logo-image" />
       </div>
 
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/about" onClick={closeMenu}>About</Link></li>
+        <li><Link to="/services" onClick={closeMenu}>Services</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
       </ul>
 
       <form className="navbar-search">
